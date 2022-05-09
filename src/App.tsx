e27a8c9fbb0efb63c9131063ef12f59ad7362a4c
zhangyuting <zhangyuting.hztianque.com>
2022-05-09 09:30:48 +0800

    todolist组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import HeaderCustom from './components/HeaderCustom';
+import { Layout, Menu } from 'antd';
+import { Outlet, useNavigate } from 'react-router-dom';
+
+const { Footer, Sider, Content } = Layout;
 
 function App() {
+  const [collapsed, setCollapsed] = React.useState(false);
+  const toggleCollapsed = () => {
+    setCollapsed(!collapsed);
+  };
+
+  
+  const navigate = useNavigate();
+  const handleMenu = (menu: any) => {
+    console.log('menu', menu)
+    navigate(menu.key)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout hasSider className="app-container">
+    <Sider
+      className="sider-container"
+      collapsible
+      trigger={null}
+      collapsed={collapsed}
+    >
+      <div className="logo" />
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={['dashboard']} onClick={handleMenu}>
+        <Menu.Item key="dashboard">
+          dashbord
+        </Menu.Item>
+        <Menu.Item key="2">
+          nav 2
+        </Menu.Item>
+        <Menu.Item key="3">
+          nav 3
+        </Menu.Item>
+      </Menu>
+    </Sider>
+    <Layout className="site-layout">
+      <HeaderCustom collapsed={collapsed} toggleCollapsed={toggleCollapsed} />
+      <Content style={{ margin: '90px 16px 0', overflow: 'initial' }}>
+        <Outlet />
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
+  </Layout>
   );
 }
 
